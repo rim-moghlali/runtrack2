f9ae22d9d30e95c47d1cedb967d12d07ee7de7ca
Rim Moghlali <114389397+rim-moghlali@users.noreply.github.com>
2022-11-18 12:28:28 +0100

    Add files via upload

---
+++ jour04/job01/index.php
@@ -0,0 +1,45 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> Job 01 - jour 04 | PHP</title>
+        <style>
+             body {
+                display: flex;
+                flex-direction: column;
+             }    
+             form{
+                display: flex;
+                flex-direction: column;
+             }
+             input{
+                max-width: 300px;
+             }
+        </style>
+
+    </head>
+
+    <body>
+        <form>
+            
+            <!--  Nom input -->
+            <label for="name"> Nom </label>
+            <input type="text" name="nom">
+                
+             <!--  Prenom input -->
+            <label for="name"> Prenom </label>
+            <input type="text" name="prenom">
+                
+             <!--  Age input -->
+            <label for="age"> Age </label>
+            <input type="text" name="age">
+            
+
+
+        </form>
+    </body>
+    
+</html>
