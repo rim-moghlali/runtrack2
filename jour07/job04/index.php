4b776179a3ee2dea011e8ab5aca6cd7f0a399167
rim-moghlali <rim-moghlali@laplateforme.io>
2022-12-02 15:36:24 +0100

    job04 calcule()

--- jour07/job04/index.php
+++ jour07/job04/index.php
@@ -1,3 +1,30 @@
 <?php
+    
+    function calcule($a, $operation, $b) {
+        switch ($operation) {
+            case '+':
+                return $a+$b;
+                break;
+            
+            case '-':
+                return $a-$b;
+                break;
+
+            case '*':
+                return $a*$b;
+                break;
+
+            case '/':
+                return $a/$b;
+                break;
+
+            case '%':
+                return $a%$b;
+                break;
+        }
+
+     }
+     $resultat = calcule(11,'*',85);
+     echo $resultat;
 
 ?>
