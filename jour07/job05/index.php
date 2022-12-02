95cb4720610d21f36e2a0d3437d5942a560951f1
rim-moghlali <rim-moghlali@laplateforme.io>
2022-12-02 19:38:48 +0100

    job 04 occurence

--- jour07/job05/index.php
+++ jour07/job05/index.php
@@ -1,3 +1,18 @@
 <?php
 
+    function occurences($str, $char) {
+        $resultat  = 0;
+        for ($a=0; isset($str[$a]); $a++) { 
+            $lettre = $str[$a];
+            if ($char == $lettre ) {
+               $resultat += 1;
+            }
+        }
+        return $resultat;
+    }
+
+    $str= "bonjour";
+    $char= "o";
+    $occ= occurences($str, $char);
+    echo $occ;
 ?>
