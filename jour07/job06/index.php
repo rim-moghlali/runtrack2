8eacb5d8212de332e796ae8022de1dbf67299f49
rim-moghlali <rim-moghlali@laplateforme.io>
2022-12-03 16:59:22 +0100

    job 6 - Leetspeak

--- jour07/job06/index.php
+++ jour07/job06/index.php
@@ -1,3 +1,33 @@
-<?php
+<?php 
 
+    $str = 'The way to get started is to quit talking and begin doing';
+
+    function leetSpeak($str){
+
+        for($i=0; isset($str[$i]); $i++){ 
+            if ($str[$i] == 'A' || $str[$i] == 'a'){ 
+                $str[$i] = '4'; 
+            }
+            elseif ($str[$i] == 'B' || $str[$i] == 'b'){
+                $str[$i] = '8';
+            }
+            elseif ($str[$i] == 'E' || $str[$i] == 'e'){
+                $str[$i] = '3';
+            }
+            elseif ($str[$i] == 'G' || $str[$i] == 'g'){
+                $str[$i] = '6';
+            }
+            elseif ($str[$i] == 'L' || $str[$i] == 'l'){
+                $str[$i] = '1';
+            }
+            elseif ($str[$i] == 'S' || $str[$i] == 's'){
+                $str[$i] = '5';
+            }
+            elseif ($str[$i] == 'T' || $str[$i] == 't'){
+                $str[$i] = '7';
+            }
+        }
+        return $str;
+    }
+    echo leetSpeak($str);
 ?>
