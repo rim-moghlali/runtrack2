c29b106db012ae2e68c3be9f2f95071debaac716
rim-moghlali <rim-moghlali@laplateforme.io>
2022-12-03 18:52:09 +0100

    base de donnees - myphpadmin

--- jour08/job01/index.php
+++ jour08/job01/index.php
@@ -1,14 +1,28 @@
 <?php
-    session_start();                       
-        if(!isset($_SESSION['nbvisites'])){
-        $_SESSION ['nbvisites'] = 0;             
-         }
+    session_start();         
+    
+    if(!isset($_SESSION['nbvisites'])) {
+        $_SESSION['nbvisites'] = 0;             
+    }
 
-        if (isset($_POST['reset'])){      
-        $_SESSION ['nbvisites'] = 0;       
-        }
+    if (isset($_GET['reset'])) {      
+        $_SESSION['nbvisites'] = 0;       
+    }
 
-        $_SESSION['nbvisites'] ++;             
+    $_SESSION['nbvisites']++;          
 
-    echo 'Le nombre des visites du site est : '. $_SESSION ['nbvisites']
+    echo 'Le nombre des visites du site est : '. $_SESSION ['nbvisites'];
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Jour08 - Job02</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <a href="?reset"><button>Reset</button></a>
+    </body>
+</html>
